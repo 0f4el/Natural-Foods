359ed0a4c80c13e71c0be46742c8ce32b2ee26fb
0f4el <rafinha.eloisio.p.g@gmail.com>
2026-07-21 18:52:35 -0300

    (feat-BE): Task 02.2 - Registro do Model Categoria no Flask-Admin com geracao automatica de Slug
    - Atualiza o model Categoria com suporte a slug automatico via eventos SQLAlchemy ; Configura e registra o CategoriaAdminView no Flask-Admin ; Define dependencias atualizadas no requirements.txt e corrige conflitos no app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from extensions import db, migrate
+from admin_config import init_admin
 
 app = Flask(__name__)
 
@@ -8,7 +9,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///NTFood.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Chave secreta (necessária para sessões e Flask-Admin)
-app.config['SECRET_KEY'] = 'natural-foods-secret-key-123'
+app.config['SECRET_KEY'] = 'natural-foods-secret-key-8681273123124'
 
 # Inicializa as extensões do banco de dados e migrações
 db.init_app(app)
@@ -17,9 +18,17 @@ migrate.init_app(app, db)
 # Importa os models para o SQLAlchemy reconhecer as tabelas
 import models
 
+# Inicializa o Flask-Admin
+init_admin(app)
+
+
+# === ROTAS ===
+
 @app.route("/")
 def hello_world():
     return render_template("teste.html")
+
+# === ----- ===
 
 if __name__ == "__main__":
     app.run(debug=True, host='127.0.0.1', port=5000)
